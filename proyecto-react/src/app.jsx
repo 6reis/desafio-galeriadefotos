05796facfba5e6f0ce0747e4c5b4cd834602458d
6reis <gracenunezr@gmail.com>
2022-12-22 15:53:55 -0300

    correcion

--- proyecto-react/src/app.jsx
+++ proyecto-react/src/app.jsx
@@ -8,9 +8,9 @@ function App() {
       <div className='contenido'>
       <Header titulo="“Galería de Imágenes con React”"/>
       <div className='imgs'>
-        <Card img="assets/imgs/naturaleza-1.jpg" title="Bosque" description="Arbol del Amor"/>
-        <Card img="assets/imgs/naturaleza-2.jpg" title="Camino verde" description="Camino de la fuerza, Sur "/>
-        <Card img="assets/imgs/naturaleza-3.jpg" title="Paisajes" description="Hermosos cerros, con area verde"/>
+        <Card img="/imgs/naturaleza-1.jpg" title="Bosque" description="Arbol del Amor"/>
+        <Card img="/imgs/naturaleza-2.jpg" title="Camino verde" description="Camino de la fuerza, Sur "/>
+        <Card img="/imgs/naturaleza-3.jpg" title="Paisajes" description="Hermosos cerros, con area verde"/>
       </div>
       <Footer />
     </div>
--- proyecto-react/src/components/Card.jsx
+++ proyecto-react/src/components/Card.jsx
@@ -2,7 +2,7 @@ const Card = (props) => {
   return (
     <>
       <div className="card">
-        <img src={props.src} className="card-img-top" alt="Card image" />
+        <img src={props.img} className="card-img-top" alt="Card image" />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.description}</p>

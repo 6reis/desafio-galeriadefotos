3b2c0afdf932c0878b5454b74e71045765a3ca37
6reis <gracenunezr@gmail.com>
2022-12-26 11:20:31 -0300

    correccion de App

---
+++ proyecto-react/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <div className='contenido'>
+      <Header titulo="“Galería de Imágenes con React”"/>
+      <div className='imgs'>
+        <Card img="/imgs/naturaleza-1.jpg" title="Bosque" description="Arbol del Amor"/>
+        <Card img="/imgs/naturaleza-2.jpg" title="Camino verde" description="Camino de la fuerza, Sur "/>
+        <Card img="/imgs/naturaleza-3.jpg" title="Paisajes" description="Hermosos cerros, con area verde"/>
+      </div>
+      <Footer />
+    </div>
+    </>
+  );
+}
+
+export default App;
